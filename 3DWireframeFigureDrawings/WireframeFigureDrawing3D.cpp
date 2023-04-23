9f4183698454175fd2aa470689532126c711df8c
CedricDeVijt <cedric.devijt2@gmail.com>
2023-04-23 11:18:25 +0200

    wireframe algic added

--- 3DWireframeFigureDrawings/WireframeFigureDrawing3D.cpp
+++ 3DWireframeFigureDrawings/WireframeFigureDrawing3D.cpp
@@ -1,7 +1,11 @@
 #include "WireframeFigureDrawing3D.h"
-#include "../objects/Figure3D.h"
-#include "../objects/TransformationMatrix.h"
+#include "WireframeFigureDrawing3DParser.h"
 #include "../objects/Line2D.h"
+#include "../objects/TransformationMatrix.h"
+#include "../SharedFunctions/DrawLines.h"
+#include "../SharedFunctions/ImageSize.h"
+#include "../SharedFunctions/ScaleLines.h"
+#include "../SharedFunctions/ProjectionFunctions.h"
 
 WireframeFigureDrawing3D::WireframeFigureDrawing3D(const ini::Configuration &configuration) {
     // Get data from configuration
@@ -12,6 +16,44 @@ WireframeFigureDrawing3D::WireframeFigureDrawing3D(const ini::Configuration &con
     int nrFigures = configuration["General"]["nrFigures"].as_int_or_die();
 
     // Get all figures from configuration
+    Figures3D figuresList = getFigures(configuration, eye, nrFigures);
+
+    // Project lines of all figures
+    Lines2D lines = ProjectionFunctions::doProjection(figuresList);
+
+    // Calculate size image
+    std::vector<int> imageSize = ImageSize::getImageSize(lines, size, x_min, x_max,y_min, y_max);
+
+    // Scale lines
+    ScaleLines::scale2DLines(lines, imageSize, x_min, x_max, y_min,  y_max);
+
+    // Make image
+    image = img::EasyImage(imageSize[0], imageSize[1], img::Color(backgroundcolor[0]*255,
+                                                                  backgroundcolor[1]*255,
+                                                                  backgroundcolor[2]*255));
+
+    // Draw lines on image
+    DrawLines::draw2DLines(image, lines);
+}
+
+Figures3D WireframeFigureDrawing3D::getFigures(const ini::Configuration &configuration, Vector3D &eye, int nrFigures) const {
     Figures3D figuresList;
-    Lines2D lines;
+    for (int i = 0; i < nrFigures; i++) {
+        // Create new figure form data
+        std::string figureName = "Figure" + std::to_string(i);
+
+        auto newFigure = WireframeFigureDrawing3DParser::parseWireframeFigureDrawing3DFigure(configuration[figureName]);
+
+        // Apply transformations to all figures (rotations, translation, scaling and eye point transformation)
+        Matrix combinedMatrix = TransformationMatrix::linedrawing3DTransformation(newFigure.getScale(), newFigure.getRotateX()*M_PI/180, newFigure.getRotateY()*M_PI/180, newFigure.getRotateZ()*M_PI/180, newFigure.getCenter(), eye);
+        newFigure.applyTransformation(combinedMatrix);
+
+        // Add figure to list
+        figuresList.push_back(newFigure);
+    }
+    return figuresList;
+}
+
+const img::EasyImage &WireframeFigureDrawing3D::getImage() const {
+    return image;
 }
--- 3DWireframeFigureDrawings/WireframeFigureDrawing3D.h
+++ 3DWireframeFigureDrawings/WireframeFigureDrawing3D.h
@@ -2,12 +2,27 @@
 #define CG_GRAPICS_ENGINE_WIREFRAMEFIGUREDRAWING3D_H
 
 
+#include <list>
 #include "../ini_configuration.h"
+#include "../easy_image.h"
+#include "../objects/vector3d.h"
+#include "../objects/Figure3D.h"
 
 class WireframeFigureDrawing3D {
 public:
-    WireframeFigureDrawing3D(const ini::Configuration &configuration);
+    explicit WireframeFigureDrawing3D(const ini::Configuration &configuration);
 
+    const img::EasyImage &getImage() const;
+
+private:
+    img::EasyImage image;
+
+    double x_max;
+    double y_max;
+    double x_min;
+    double y_min;
+
+    std::list<Figure3D>getFigures(const ini::Configuration &configuration, Vector3D &eye, int nrFigures) const;
 };
 
 
