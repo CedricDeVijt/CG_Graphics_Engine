c3d37b9c88ac2530e098bc21f2a0330db5dbb9f7
CedricDeVijt <cedric.devijt2@gmail.com>
2023-04-23 11:16:29 +0200

    Static function for drawing lines added

---
+++ SharedFunctions/DrawLines.h
@@ -0,0 +1,13 @@
+#ifndef CG_GRAPICS_ENGINE_DRAWLINES_H
+#define CG_GRAPICS_ENGINE_DRAWLINES_H
+
+
+#include "../objects/Line2D.h"
+
+class DrawLines {
+public:
+    static void draw2DLines(img::EasyImage &image, Lines2D lines);
+};
+
+
+#endif //CG_GRAPICS_ENGINE_DRAWLINES_H
