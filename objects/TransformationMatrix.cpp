bb2839ea4899255cab24ce3cad026fc9842d9657
CedricDeVijt <cedric.devijt2@gmail.com>
2023-04-23 11:19:01 +0200

    format changes to TransformationMatrix.cpp

--- objects/TransformationMatrix.cpp
+++ objects/TransformationMatrix.cpp
@@ -6,7 +6,6 @@ Matrix TransformationMatrix::scaleFigure(const double &scale) {
     matrix(1,1) = scale;
     matrix(2,2) = scale;
     matrix(3,3) = scale;
-    matrix(4,4) = 1;
 
     return matrix;
 }
@@ -14,12 +13,10 @@ Matrix TransformationMatrix::scaleFigure(const double &scale) {
 Matrix TransformationMatrix::rotateX(const double &angle) {
     Matrix matrix;
 
-    matrix(1,1) = 1;
     matrix(2,2) = cos(angle);
     matrix(2,3) = sin(angle);
     matrix(3,2) = -sin(angle);
     matrix(3,3) = cos(angle);
-    matrix(4,4) = 1;
 
     return matrix;
 }
@@ -29,10 +26,8 @@ Matrix TransformationMatrix::rotateY(const double &angle) {
 
     matrix(1,1) = cos(angle);
     matrix(1,3) = -sin(angle);
-    matrix(2,2) = 1;
     matrix(3,1) = sin(angle);
     matrix(3,3) = cos(angle);
-    matrix(4,4) = 1;
 
     return matrix;
 }
@@ -44,19 +39,12 @@ Matrix TransformationMatrix::rotateZ(const double &angle) {
     matrix(1,2) = sin(angle);
     matrix(2,1) = -sin(angle);
     matrix(2,2) = cos(angle);
-    matrix(3,3) = 1;
-    matrix(4,4) = 1;
 
     return matrix;
 }
 
 Matrix TransformationMatrix::translateFigure(const Vector3D &vector) {
     Matrix matrix;
-
-    matrix(1,1) = 1;
-    matrix(2,2) = 1;
-    matrix(3,3) = 1;
-    matrix(4,4) = 1;
 
     matrix(4,1) = vector.x;
     matrix(4,2) = vector.y;
@@ -83,8 +71,6 @@ Matrix TransformationMatrix::eyePointTransformation(const Vector3D &eyepoint) {
     matrix(3,3) = cos(phi);
 
     matrix(4,3) = -r;
-    matrix(4,4) = 1;
-
 
     return matrix;
 }
@@ -93,9 +79,12 @@ Matrix
 TransformationMatrix::linedrawing3DTransformation(const double &scale, const double &angleX, const double &angleY,
                                                   const double &angleZ, const Vector3D &vector, Vector3D eye) {
 
-    Matrix matrix = TransformationMatrix::scaleFigure(scale) * TransformationMatrix::rotateX(angleX)
-            * TransformationMatrix::rotateY(angleY) * TransformationMatrix::rotateZ(angleZ)
-            * TransformationMatrix::translateFigure(vector) * TransformationMatrix::eyePointTransformation(eye);
+    Matrix matrix = TransformationMatrix::scaleFigure(scale)
+            * TransformationMatrix::rotateX(angleX)
+            * TransformationMatrix::rotateY(angleY)
+            * TransformationMatrix::rotateZ(angleZ)
+            * TransformationMatrix::translateFigure(vector)
+            * TransformationMatrix::eyePointTransformation(eye);
     return matrix;
 }
 
