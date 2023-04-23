1c606b31eb34f2f90496ff2010153c35e6a51606
CedricDeVijt <cedric.devijt2@gmail.com>
2023-04-23 11:16:53 +0200

    Static function for calculating image size added

---
+++ SharedFunctions/ImageSize.cpp
@@ -0,0 +1,40 @@
+#include "ImageSize.h"
+
+std::vector<int> ImageSize::getImageSize(Lines2D lines, int size, double &x_min, double &x_max, double &y_min, double &y_max){
+    x_max = -INFINITY;
+    y_max = -INFINITY;
+    x_min = INFINITY;
+    y_min = INFINITY;
+
+    for (const Line2D& line : lines){
+        // x_max
+        x_max = std::max(line.pointA.x, x_max);
+        x_max = std::max(line.pointB.x, x_max);
+
+        // x_min
+        x_min = std::min(line.pointA.x, x_min);
+        x_min = std::min(line.pointB.x, x_min);
+
+        // y_max
+        y_max = std::max(line.pointA.y, y_max);
+        y_max = std::max(line.pointB.y, y_max);
+
+
+        // y_min
+        y_min = std::min(line.pointA.y, y_min);
+        y_min = std::min(line.pointB.y, y_min);
+    }
+
+    // calculate range for x en y
+    double range_x = x_max - x_min;
+    double range_y = y_max - y_min;
+
+    // calculate max range
+    double max_range = std::max(range_x, range_y);
+
+    // calculate size of image
+    double image_x = size*(range_x/max_range);
+    double image_y = size*(range_y/max_range);
+
+    return {int(image_x), int(image_y)};
+}
---
+++ SharedFunctions/ImageSize.h
@@ -0,0 +1,13 @@
+#ifndef CG_GRAPICS_ENGINE_IMAGESIZE_H
+#define CG_GRAPICS_ENGINE_IMAGESIZE_H
+
+
+#include "../objects/Line2D.h"
+
+class ImageSize {
+public:
+    static std::vector<int> getImageSize(Lines2D lines, int size, double &x_min, double &x_max, double &y_min, double &y_max);
+};
+
+
+#endif //CG_GRAPICS_ENGINE_IMAGESIZE_H
