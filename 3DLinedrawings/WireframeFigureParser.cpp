ae6d8cfcdcafd4092711c41acf108310a67cc83f
CedricDeVijt <cedric.devijt2@gmail.com>
2023-05-03 15:55:18 +0200

    Bug fixes + added functionality

--- 3DLinedrawings/WireframeFigureParser.cpp
+++ 3DLinedrawings/WireframeFigureParser.cpp
@@ -1,61 +1,71 @@
 #include <fstream>
+#include <cmath>
+
 #include "WireframeFigureParser.h"
 #include "../l_parser.h"
+#include "../Objects/Line2D.h"
+#include "../SharedFunctions/LSystemFunctions.h"
 
-Figure3D WireframeFigureParser::parseWireframeFigureDrawing3DFigure(const ini::Section &figure) {
+Figure3D WireframeFigureParser::parseWireframeFigure(const ini::Section &figure) {
+    // Get data from config
     std::string type = figure["type"].as_string_or_die();
 
-    double rotateX = figure["rotateX"].as_int_or_die();
-    double rotateY = figure["rotateY"].as_int_or_die();
-    double rotateZ = figure["rotateZ"].as_int_or_die();
-    std::vector<double> rotations = {rotateX, rotateY, rotateZ};
+    double rotateX = figure["rotateX"].as_double_or_die();
+    double rotateY = figure["rotateY"].as_double_or_die();
+    double rotateZ = figure["rotateZ"].as_double_or_die();
+    const std::vector<double> &rotations = {rotateX, rotateY, rotateZ};
 
-    double scale = figure["scale"].as_double_or_die();
-    auto centerTuple = figure["center"].as_int_tuple_or_die();
+    const double &scale = figure["scale"].as_double_or_die();
+
+    auto centerTuple = figure["center"].as_double_tuple_or_die();
     Vector3D center = Vector3D::vector(centerTuple[0], centerTuple[1], centerTuple[2]);
+
     std::vector<double> figureColor = figure["color"].as_double_tuple_or_die();
     img::Color color = img::Color(figureColor[0]*255, figureColor[1]*255, figureColor[2]*255);
 
+    double R = figure["R"].as_double_or_default(0);
+    double r = figure["r"].as_double_or_default(0);
+    int n = figure["n"].as_int_or_default(0);
+    double m = figure["m"].as_double_or_default(0);
+
+    double height = figure["height"].as_double_or_default(0);
+
+    // Create correct figure
     if (type == "LineDrawing") {
         return WireframeFigureParser::parseLineDrawing(figure, rotations, scale, center, color);
     }else if (type == "3DLSystem"){
         return WireframeFigureParser::parse3DLSystem(figure, rotations, scale, center, color);
     } else if (type == "Cube"){
-        return WireframeFigureParser::parseCube(figure, rotations, scale, center, color);
+        return WireframeFigureParser::parseCube(rotations, scale, center, color);
     } else if (type == "Tetrahedron"){
-        return WireframeFigureParser::parseTetrahedron(figure, rotations, scale, center, color);
+        return WireframeFigureParser::parseTetrahedron(rotations, scale, center, color);
     } else if (type == "Octahedron"){
-        return WireframeFigureParser::parseOctahedron(figure, rotations, scale, center, color);
+        return WireframeFigureParser::parseOctahedron(rotations, scale, center, color);
     } else if (type == "Icosahedron"){
-        return WireframeFigureParser::parseIcosahedron(figure, rotations, scale, center, color);
-//    } else if (type == "Dodecahedron"){
-//        return WireframeFigureParser::parseDodecahedron(figure, rotations, scale, center, color);
-//    } else if (type == "Cylinder"){
-//        return WireframeFigureParser::parseCylinder(figure, rotations, scale, center, color);
-//    } else if (type == "Cone"){
-//        return WireframeFigureParser::parseCone(figure, rotations, scale, center, color);
-//    } else if (type == "Sphere"){
-//        return WireframeFigureParser::parseSphere(figure, rotations, scale, center, color);
-//    } else if (type == "Torus"){
-//        return WireframeFigureParser::parseTorus(figure, rotations, scale, center, color);
-//    } else {
-//        return Figure3D(__1::vector(), __1::vector(), __1::vector(), 0, Vector3D(), img::Color());
+        return WireframeFigureParser::parseIcosahedron(rotations, scale, center, color);
+    } else if (type == "Dodecahedron"){
+        return WireframeFigureParser::parseDodecahedron(rotations, scale, center, color);
+    } else if (type == "Cylinder"){
+        return WireframeFigureParser::parseCylinder(rotations, scale, center, color, n, height);
+    } else if (type == "Cone"){
+        return WireframeFigureParser::parseCone(rotations, scale, center, color, n, height);
+    } else if (type == "Sphere"){
+        return WireframeFigureParser::parseSphere(rotations, scale, center, color, n);
+    } else if (type == "Torus"){
+        return WireframeFigureParser::parseTorus(rotations, scale, center, color, R, r, n, m);
     } else {
-        std::vector<Face3D> faces;
-        std::vector<Vector3D> points;
-        return {faces, points, rotations, scale, center, color};
+        return {};
     }
 }
 
-Figure3D WireframeFigureParser::parseLineDrawing(const ini::Section &figure, std::vector<double> rotations, double scale,
-                                                 const Vector3D &center, const img::Color &color) {
+Figure3D WireframeFigureParser::parseLineDrawing(const ini::Section &figure, const std::vector<double> &rotations, const double &scale, const Vector3D &center, const img::Color &color) {
     std::vector<Face3D> faces;
     std::vector<Vector3D> points;
 
     int nrPoints = figure["nrPoints"].as_int_or_die();
     int nrLines = figure["nrLines"].as_int_or_die();
 
-    // get all points
+    // Points
     for (int i = 0; i < nrPoints; i++) {
         std::string pointName = "point" + std::to_string(i);
         auto figurePoint = figure[pointName].as_double_tuple_or_die();
@@ -63,7 +73,7 @@ Figure3D WireframeFigureParser::parseLineDrawing(const ini::Section &figure, std
         points.emplace_back(newPoint);
     }
 
-    // get all faces
+    // Faces
     for (int i = 0; i < nrLines; i++) {
         std::string lineName = "line" + std::to_string(i);
         ini::IntTuple figureLine = figure[lineName].as_int_tuple_or_die();
@@ -74,47 +84,27 @@ Figure3D WireframeFigureParser::parseLineDrawing(const ini::Section &figure, std
     return {faces, points, rotations, scale, center, color};
 }
 
-Figure3D WireframeFigureParser::parse3DLSystem(const ini::Section &figure, std::vector<double> rotations, double scale,
-                                               const Vector3D &center, const img::Color &color) {
+Figure3D WireframeFigureParser::parse3DLSystem(const ini::Section &figure, const std::vector<double> &rotations, const double &scale, const Vector3D &center, const img::Color &color) {
     // Get input file
     std::string inputfile = figure["inputfile"].as_string_or_die();
 
     // Parse L-system from inputfile
-    LParser::LSystem3D l_system;
-    std::ifstream input_stream(inputfile);
-    input_stream >> l_system;
-    input_stream.close();
+    LParser::LSystem3D l_system = LSystemFunctions::parseLSystem3D(inputfile);
 
     // Calculate string
-    std::string oldString = l_system.get_initiator();
-    std::string figureString;
-    if (l_system.get_nr_iterations() == 0){
-        figureString = oldString;
-    } else {
-        for (int i = 0; i < l_system.get_nr_iterations(); ++i) {
-            figureString = "";
-            for (char c: oldString) {
-                if (l_system.get_alphabet().find(c) != l_system.get_alphabet().end()){
-                    figureString += l_system.get_replacement(c);
-                } else {
-                    figureString += c;
-                }
-            }
-            oldString = figureString;
-        }
-    }
+    std::string figureString = LSystemFunctions::getLSystemString(l_system);
 
     // Get all lines
+    std::pair<std::vector<Face3D>, std::vector<Vector3D>> lines = LSystemFunctions::getFacesAndPoints(l_system, figureString);
 
-
-
-    return Figure3D();
+    return {lines.first, lines.second, rotations, scale, center, color};
 }
 
-Figure3D WireframeFigureParser::parseCube(const ini::Section& figure, std::vector<double> rotations, double scale, const Vector3D& center, const img::Color& color) {
+Figure3D WireframeFigureParser::parseCube(const std::vector<double> &rotations, const double &scale, const Vector3D& center, const img::Color& color) {
     std::vector<Face3D> faces;
     std::vector<Vector3D> points;
 
+    // Points
     points.emplace_back(Vector3D::point(1, -1, -1));
     points.emplace_back(Vector3D::point(-1, 1, -1));
     points.emplace_back(Vector3D::point(1, 1, 1));
@@ -124,6 +114,7 @@ Figure3D WireframeFigureParser::parseCube(const ini::Section& figure, std::vecto
     points.emplace_back(Vector3D::point(1, -1, 1));
     points.emplace_back(Vector3D::point(-1, 1, 1));
 
+    // Faces
     faces.emplace_back(Face3D({0, 4, 2, 6}));
     faces.emplace_back(Face3D({4, 1, 7, 2}));
     faces.emplace_back(Face3D({1, 5, 3, 7}));
@@ -131,32 +122,33 @@ Figure3D WireframeFigureParser::parseCube(const ini::Section& figure, std::vecto
     faces.emplace_back(Face3D({6, 2, 7, 3}));
     faces.emplace_back(Face3D({0, 5, 1, 4}));
 
-
     return {faces, points, rotations, scale, center, color};
 }
 
-Figure3D WireframeFigureParser::parseTetrahedron(const ini::Section& figure, std::vector<double> rotations, double scale, const Vector3D& center, const img::Color& color) {
+Figure3D WireframeFigureParser::parseTetrahedron(const std::vector<double> &rotations, const double &scale, const Vector3D& center, const img::Color& color) {
     std::vector<Face3D> faces;
     std::vector<Vector3D> points;
 
+    // Points
     points.emplace_back(Vector3D::point(1, -1, -1));
     points.emplace_back(Vector3D::point(-1, 1, -1));
     points.emplace_back(Vector3D::point(1, 1, 1));
     points.emplace_back(Vector3D::point(-1, -1, 1));
 
+    // Faces
     faces.emplace_back(Face3D({0, 1, 2}));
     faces.emplace_back(Face3D({1, 3, 2}));
     faces.emplace_back(Face3D({0, 3, 1}));
     faces.emplace_back(Face3D({0, 2, 3}));
 
-
     return {faces, points, rotations, scale, center, color};
 }
 
-Figure3D WireframeFigureParser::parseOctahedron(const ini::Section& figure, std::vector<double> rotations, double scale, const Vector3D& center, const img::Color& color) {
+Figure3D WireframeFigureParser::parseOctahedron(const std::vector<double> &rotations, const double &scale, const Vector3D& center, const img::Color& color) {
     std::vector<Face3D> faces;
     std::vector<Vector3D> points;
 
+    // Points
     points.emplace_back(Vector3D::point(1, 0, 0));
     points.emplace_back(Vector3D::point(0, 1, 0));
     points.emplace_back(Vector3D::point(-1, 0, 0));
@@ -164,6 +156,7 @@ Figure3D WireframeFigureParser::parseOctahedron(const ini::Section& figure, std:
     points.emplace_back(Vector3D::point(0, 0, -1));
     points.emplace_back(Vector3D::point(0, 0, 1));
 
+    // Faces
     faces.emplace_back(Face3D({0, 1, 5}));
     faces.emplace_back(Face3D({1, 2, 5}));
     faces.emplace_back(Face3D({2, 3, 5}));
@@ -177,12 +170,12 @@ Figure3D WireframeFigureParser::parseOctahedron(const ini::Section& figure, std:
     return {faces, points, rotations, scale, center, color};
 }
 
-Figure3D WireframeFigureParser::parseIcosahedron(const ini::Section& figure, std::vector<double> rotations, double scale, const Vector3D& center, const img::Color& color) {
+Figure3D WireframeFigureParser::parseIcosahedron(const std::vector<double> &rotations, const double &scale, const Vector3D& center, const img::Color& color) {
     std::vector<Face3D> faces;
     std::vector<Vector3D> points;
 
-    points.emplace_back(Vector3D::point(0, 0, sqrt(5) / 2));
-
+    // Points
+    points.emplace_back(Vector3D::point(0, 0, (sqrt(5)) / 2));
     for (int i = 1; i < 6; ++i) {
         points.emplace_back(Vector3D::point(cos((i - 2) * 2 * M_PI / 5), sin((i - 2) * 2 * M_PI / 5), 0.5));
     }
@@ -192,6 +185,7 @@ Figure3D WireframeFigureParser::parseIcosahedron(const ini::Section& figure, std
     }
     points.emplace_back(Vector3D::point(0, 0, -sqrt(5) / 2));
 
+    // Faces
     faces.emplace_back(Face3D({0, 1, 2}));
     faces.emplace_back(Face3D({0, 2, 3}));
     faces.emplace_back(Face3D({0, 3, 4}));
@@ -206,14 +200,162 @@ Figure3D WireframeFigureParser::parseIcosahedron(const ini::Section& figure, std
     faces.emplace_back(Face3D({4, 9, 5}));
     faces.emplace_back(Face3D({5, 9, 10}));
     faces.emplace_back(Face3D({5, 10, 1}));
-    faces.emplace_back(Face3D({1, 10, 11}));
-    faces.emplace_back(Face3D({12, 8, 7}));
-    faces.emplace_back(Face3D({12, 9, 8}));
-    faces.emplace_back(Face3D({12, 10, 9}));
-    faces.emplace_back(Face3D({12, 11, 10}));
-    faces.emplace_back(Face3D({12, 7, 11}));
+    faces.emplace_back(Face3D({1, 10, 6}));
+    faces.emplace_back(Face3D({11, 7, 6}));
+    faces.emplace_back(Face3D({11, 8, 7}));
+    faces.emplace_back(Face3D({11, 9, 8}));
+    faces.emplace_back(Face3D({11, 10, 9}));
+    faces.emplace_back(Face3D({11, 6, 10}));
+
 
 
     return {faces, points, rotations, scale, center, color};
 }
 
+Figure3D WireframeFigureParser::parseDodecahedron(const std::vector<double> &rotations, const double &scale, const Vector3D &center, const img::Color &color) {
+    std::vector<Face3D> faces;
+    std::vector<Vector3D> points;
+
+    // Points
+    Figure3D icosahedron = parseIcosahedron({0,0,0}, 1, Vector3D::vector(0,0,0), color);
+    for (const Face3D &face : icosahedron.faces){
+        double x = (icosahedron.points[face.point_indexes[0]].x +
+                    icosahedron.points[face.point_indexes[1]].x +
+                    icosahedron.points[face.point_indexes[2]].x) / 3;
+
+        double y = (icosahedron.points[face.point_indexes[0]].y +
+                    icosahedron.points[face.point_indexes[1]].y +
+                    icosahedron.points[face.point_indexes[2]].y) / 3;
+
+        double z = (icosahedron.points[face.point_indexes[0]].z +
+                    icosahedron.points[face.point_indexes[1]].z +
+                    icosahedron.points[face.point_indexes[2]].z) / 3;
+
+        points.push_back(Vector3D::point(x, y, z));
+    }
+
+    // Faces
+    faces.emplace_back(Face3D({0, 1, 2, 3, 4}));
+    faces.emplace_back(Face3D({0, 5, 6, 7, 1}));
+    faces.emplace_back(Face3D({1, 7, 8, 9, 2}));
+    faces.emplace_back(Face3D({2, 9, 10, 11, 3}));
+    faces.emplace_back(Face3D({3, 11, 12, 13, 4}));
+    faces.emplace_back(Face3D({4, 13, 14, 5, 0}));
+    faces.emplace_back(Face3D({19, 18, 17, 16, 15}));
+    faces.emplace_back(Face3D({19, 14, 13, 12, 18}));
+    faces.emplace_back(Face3D({18, 12, 11, 10, 17}));
+    faces.emplace_back(Face3D({17, 10, 9, 8, 16}));
+    faces.emplace_back(Face3D({16, 8, 7, 6, 15}));
+    faces.emplace_back(Face3D({15, 6, 5, 14, 19}));
+
+    return {faces, points, rotations, scale, center, color};
+}
+
+Figure3D WireframeFigureParser::parseCylinder(const std::vector<double> &rotations, const double &scale, const Vector3D& center, const img::Color& color, const int &n, const double &height) {
+    std::vector<Face3D> faces;
+    std::vector<Vector3D> points;
+
+    // Points
+    for (int i = 0; i < n; ++i) {
+        points.push_back(Vector3D::point(cos(2 * i * M_PI / n), sin(2 * i * M_PI / n), height));
+        points.push_back(Vector3D::point(cos(2 * i * M_PI / n), sin(2 * i * M_PI / n), 0));
+    }
+
+    // Faces
+    for (int i = 0; i < n; ++i) {
+        faces.push_back(Face3D({2*i, 2*i+1, (2*i+3) % (2*n), (2*i+2) % (2*n)}));
+    }
+    std::vector<int> topFace;
+    std::vector<int> bottomFace;
+    for (int i = 0; i < n; ++i) {
+        topFace.push_back(2*i);
+        bottomFace.push_back(2*i+1);
+    }
+    faces.emplace_back(topFace);
+    faces.emplace_back(bottomFace);
+
+    return {faces, points, rotations, scale, center, color};
+}
+
+Figure3D WireframeFigureParser::parseCone(const std::vector<double> &rotations, const double &scale, const Vector3D& center, const img::Color& color, const int &n, const double &height) {
+    std::vector<Face3D> faces;
+    std::vector<Vector3D> points;
+
+    // Points
+    for (int i = 0; i < n; ++i) {
+        points.push_back(Vector3D::point(cos(2 * i * M_PI / n), sin(2 * i * M_PI / n), 0));
+    }
+    points.emplace_back(Vector3D::point(0, 0, height));
+
+    // Faces
+    for (int i = 0; i < n; ++i) {
+        faces.emplace_back(Face3D({n, i % n, (i + 1) % n}));
+    }
+    std::vector<int> topFace;
+    for (int i = 0; i < n; ++i) {
+        topFace.push_back(i);
+    }
+
+    return {faces, points, rotations, scale, center, color};
+}
+
+Figure3D WireframeFigureParser::parseSphere(const std::vector<double> &rotations, const double &scale, const Vector3D& center, const img::Color& color, const int &n) {
+    std::vector<Face3D> faces;
+    std::vector<Vector3D> points;
+
+    if (n == 0){
+        return parseIcosahedron(rotations, scale, center, color);
+    } else {
+        Figure3D icosahedron = parseIcosahedron({0,0,0}, 1, Vector3D::vector(0,0,0), color);
+
+        // Split icosahedron n-times
+        for (int i = 0; i < n; ++i) {
+            std::vector<Face3D> newFaces;
+            // Split each face
+            for (int j = 0; j < faces.size(); ++j) {
+                Face3D face = icosahedron.faces[i];
+
+                // Current point of triangle
+                const Vector3D& A = icosahedron.points[face.point_indexes[0]];
+                const Vector3D& B = icosahedron.points[face.point_indexes[1]];
+                const Vector3D& C = icosahedron.points[face.point_indexes[2]];
+
+                // New split points of triangle
+                Vector3D D = Vector3D::point((A.x + B.x)/2,
+                                             (A.y + B.y)/2,
+                                             (A.z + B.z)/2);
+                Vector3D E = Vector3D::point((A.x + C.x)/2,
+                                             (A.y + C.y)/2,
+                                             (A.z + C.z)/2);
+                Vector3D F = Vector3D::point((B.x + C.x)/2,
+                                             (B.y + C.y)/2,
+                                             (B.z + C.z)/2);
+
+                icosahedron.points.push_back(D);
+                icosahedron.points.push_back(E);
+                icosahedron.points.push_back(F);
+
+                int Ai = face.point_indexes[0];
+                int Bi = face.point_indexes[1];
+                int Ci = face.point_indexes[2];
+                int Di = icosahedron.points.size()-3;
+                int Ei = icosahedron.points.size()-2;
+                int Fi = icosahedron.points.size()-1;
+
+                newFaces.push_back(Face3D({Ai, Di, Ei}));
+                newFaces.push_back(Face3D({Bi, Fi, Di}));
+                newFaces.push_back(Face3D({Ci, Ei, Fi}));
+                newFaces.push_back(Face3D({Di, Fi, Ei}));
+            }
+
+            icosahedron.faces = newFaces;
+        }
+
+    return {icosahedron.faces, icosahedron.points, rotations, scale, center, color};
+    }
+
+}
+
+Figure3D WireframeFigureParser::parseTorus(const std::vector<double> &rotations, const double &scale, const Vector3D& center, const img::Color& color, const double &R, const double &r, const int &n, const int &m) {
+    return Figure3D();
+}
--- 3DLinedrawings/WireframeFigureParser.h
+++ 3DLinedrawings/WireframeFigureParser.h
@@ -1,27 +1,26 @@
 #ifndef CG_GRAPICS_ENGINE_WIREFRAMEFIGUREPARSER_H
 #define CG_GRAPICS_ENGINE_WIREFRAMEFIGUREPARSER_H
 
-#include <cmath>
 #include "../ini_configuration.h"
 #include "../Objects/Figure3D.h"
 
 class WireframeFigureParser {
 public:
 
-    static Figure3D parseWireframeFigureDrawing3DFigure(const ini::Section& figure);
+    static Figure3D parseWireframeFigure(const ini::Section& figure);
 
 private:
-    static Figure3D parseLineDrawing(const ini::Section& figure, std::vector<double> rotations, double scale, const Vector3D& center, const img::Color& color);
-    static Figure3D parse3DLSystem(const ini::Section& figure, std::vector<double> rotations, double scale, const Vector3D& center, const img::Color& color);
-    static Figure3D parseCube(const ini::Section& figure, std::vector<double> rotations, double scale, const Vector3D& center, const img::Color& color);
-    static Figure3D parseTetrahedron(const ini::Section& figure, std::vector<double> rotations, double scale, const Vector3D& center, const img::Color& color);
-    static Figure3D parseOctahedron(const ini::Section& figure, std::vector<double> rotations, double scale, const Vector3D& center, const img::Color& color);
-    static Figure3D parseIcosahedron(const ini::Section& figure, std::vector<double> rotations, double scale, const Vector3D& center, const img::Color& color);
-    static Figure3D parseDodecahedron(const ini::Section& figure, std::vector<double> rotations, double scale, const Vector3D& center, const img::Color& color);
-    static Figure3D parseCylinder(const ini::Section& figure, std::vector<double> rotations, double scale, const Vector3D& center, const img::Color& color);
-    static Figure3D parseCone(const ini::Section& figure, std::vector<double> rotations, double scale, const Vector3D& center, const img::Color& color);
-    static Figure3D parseSphere(const ini::Section& figure, std::vector<double> rotations, double scale, const Vector3D& center, const img::Color& color);
-    static Figure3D parseTorus(const ini::Section& figure, std::vector<double> rotations, double scale, const Vector3D& center, const img::Color& color);
+    static Figure3D parseLineDrawing(const ini::Section& figure, const std::vector<double> &rotations, const double &scale, const Vector3D& center, const img::Color& color);
+    static Figure3D parse3DLSystem(const ini::Section& figure, const std::vector<double> &rotations, const double &scale, const Vector3D& center, const img::Color& color);
+    static Figure3D parseCube(const std::vector<double> &rotations, const double &scale, const Vector3D& center, const img::Color& color);
+    static Figure3D parseTetrahedron(const std::vector<double> &rotations, const double &scale, const Vector3D& center, const img::Color& color);
+    static Figure3D parseOctahedron(const std::vector<double> &rotations, const double &scale, const Vector3D& center, const img::Color& color);
+    static Figure3D parseIcosahedron(const std::vector<double> &rotations, const double &scale, const Vector3D& center, const img::Color& color);
+    static Figure3D parseDodecahedron(const std::vector<double> &rotations, const double &scale, const Vector3D& center, const img::Color& color);
+    static Figure3D parseCylinder(const std::vector<double> &rotations, const double &scale, const Vector3D& center, const img::Color& color, const int &n, const double &height);
+    static Figure3D parseCone(const std::vector<double> &rotations, const double &scale, const Vector3D& center, const img::Color& color, const int &n, const double &height);
+    static Figure3D parseSphere(const std::vector<double> &rotations, const double &scale, const Vector3D& center, const img::Color& color, const int &n);
+    static Figure3D parseTorus(const std::vector<double> &rotations, const double &scale, const Vector3D& center, const img::Color& color, const double &R, const double &r, const int &n, const int &m);
 };
 
 
